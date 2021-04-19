03dfcfbaccb479735df8dc71f9803df4759dcdbe
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-04-19 17:33:01 -0300

    Tela inicial desenvolvida.

--- plantmanager/App.tsx
+++ plantmanager/App.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Welcome } from './src/pages/Welcome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Olá!</Text>
-      <Text>NLW5</Text>
-    </View>
+    <Welcome />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
