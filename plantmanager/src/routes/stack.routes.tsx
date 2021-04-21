e3f5614ef40c47b204ef526bbb7253f6f6024a6b
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-04-21 11:58:03 -0300

    Cabeçalho.

--- plantmanager/src/routes/stack.routes.tsx
+++ plantmanager/src/routes/stack.routes.tsx
@@ -6,6 +6,7 @@ import colors from '../styles/colors'
 import { Welcome } from '../pages/Welcome'
 import { UserIdentification } from '../pages/UserIdentification'
 import { Confirmation } from '../pages/Confirmation'
+import { PlantSelect } from '../pages/PlantSelect'
 
 const StackRoutes = createStackNavigator()
 
@@ -27,9 +28,15 @@ const AppRoutes: React.FC = () => (
       name="UserIdentification"
       component={UserIdentification}
     />
+
     <StackRoutes.Screen
       name="Confirmation"
       component={Confirmation}
+    />
+
+    <StackRoutes.Screen 
+      name="PlantSelect"
+      component={PlantSelect}
     />
 
   </StackRoutes.Navigator>
